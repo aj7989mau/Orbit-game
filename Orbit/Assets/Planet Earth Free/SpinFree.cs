9481db410c3f7814648533877662a5cac86132db
ktgktg <ktgktg@hotmail.com>
2021-04-16 12:43:13 +0200

    Demo level added

---
+++ Orbit/Assets/Planet Earth Free/SpinFree.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Spin the object at a specified speed
+/// </summary>
+public class SpinFree : MonoBehaviour 
+{
+	public bool spin;
+	public float speed = 10f;
+	////[HideInInspector]
+	//public float direction = 1f;
+	////[HideInInspector]
+	//public float directionChangeSpeed = 2f;
+
+	void Update() 
+	{
+		//if (direction < 1f) 
+		//{
+		//	direction += Time.deltaTime / (directionChangeSpeed / 2);
+		//}
+
+		if (spin) 
+		{
+			transform.Rotate(Vector3.up, speed * Time.deltaTime); 
+		}
+	}
+}
---
+++ Orbit/Assets/PlayerMovement.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public bool spin;
+	public float speed = 1f;
+	public GameObject target;
+
+	void Update()
+	{
+		if (spin)
+		{
+			transform.RotateAround(target.transform.position, Vector3.up, -speed * Time.deltaTime);
+			//transform.Rotate(Vector3.up, speed * Time.deltaTime);
+		}
+	}
+}
