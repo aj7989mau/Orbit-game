604d20b6879af13af28ef6f922dfd297bfaca6ec
ktgktg <ktgktg@hotmail.com>
2021-04-20 20:41:16 +0200

    Player control fixed

--- Orbit/Assets/PlayerMovement.cs
+++ Orbit/Assets/PlayerMovement.cs
@@ -3,16 +3,23 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
-	public bool spin;
-	public float speed = 1f;
-	public GameObject target;
+	public float orbitSpeed = 2f;
+    public float yawSpeed = 5f;
+    public GameObject orbitCenter;
+	public GameObject playerObject;
 
-	void Update()
-	{
-		if (spin)
-		{
-			transform.RotateAround(target.transform.position, Vector3.forward, -speed * Time.deltaTime);
-			//transform.Rotate(Vector3.up, speed * Time.deltaTime);
-		}
-	}
+    void Update()
+    {
+        transform.RotateAround(orbitCenter.transform.position, playerObject.transform.forward, -orbitSpeed * Time.deltaTime);
+
+        if (Input.GetKey("a"))
+        {
+            transform.RotateAround(orbitCenter.transform.position, playerObject.transform.position, -yawSpeed * Time.deltaTime);
+        }
+
+        else if (Input.GetKey("d"))
+        {
+            transform.RotateAround(orbitCenter.transform.position, -playerObject.transform.position, -yawSpeed * Time.deltaTime);
+        }
+    }
 }
